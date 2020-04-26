90ef277453dbb15b69a1cccea21ecf22e2dd5dcf
Josias Kayan Lima de Carvalho <josias.carvalho@elsys.com.br>
2020-04-26 19:44:13 -0400

    Teste 4

---
+++ build/precache-manifest.9ef90ccc033bccb92cf01c93c70a8188.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bcfd3623eb4278a92f927b7a3e31855",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c33ebb2e66f4ccb56f8",
+    "url": "/static/css/main.802b8e2d.chunk.css"
+  },
+  {
+    "revision": "38080cec06d12963b304",
+    "url": "/static/js/2.1376ec01.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1376ec01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c33ebb2e66f4ccb56f8",
+    "url": "/static/js/main.24e88732.chunk.js"
+  },
+  {
+    "revision": "45a62228ec41ca58a1d8",
+    "url": "/static/js/runtime-main.8c898ed3.js"
+  },
+  {
+    "revision": "a2fdced09093541bd446751cb54a87e7",
+    "url": "/static/media/pokedex.a2fdced0.jpg"
+  }
+]);
